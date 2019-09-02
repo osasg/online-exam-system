87cb5311c7d9970b7f51a6d19b2eddd42e9eebfd
Quang Dat <quangdat2000.pham@gmail.com>
2019-09-02 22:01:47 +0700

    update author

--- server/app/routes/root.route.js
+++ server/app/routes/root.route.js
@@ -7,68 +7,14 @@ const examRoute = require('./exam.route')
 const subjectRoute = require('./subject.route');
 const examHistoryRoute = require('./exam-history.route');
 
+const author = require('../../../author.json');
+
 router.get('/', (req, res) => {
   res.send({
     success: true,
     description: 'Welcome to online-exam system',
     repository: 'https://github.com/quangdatpham/online-exam-system',
-    author: {
-      fullname: 'Quang Dat Pham',
-      dob: '07/15/2000',
-      nationality: 'Vietnamese',
-      role: 'Full-stack Web Developer',
-      description: `Curious - Ambitious - Adventurous
-I'm a well self-study person. A responsible person, always actively learning, ambitious and eager for dedication.
-I love sharing my knowledge. It is not property and therefore can not be owned by anyone.`,
-      professionalSkills: [
-        'Advanced JavaScript',
-        'NodeJS',
-        'ReactJS',
-        'RubyOnRails',
-        'Bash scripting'
-      ],
-      interests: [
-        'traveling', 'programming'
-      ],
-      skills: {
-        frontend: [
-          'Bootstrap',
-          'Redux',
-          'Responsive Web Design',
-          'DOM'
-        ],
-        backend: [
-          'SpringMVC & Hibernate',
-          'OOP',
-          'Spree Commerce',
-          'gRPC',
-          'Microservices architecture'
-        ],
-        webDesign: [
-          'UI-UX',
-          'Sketch',
-          'XD',
-          'Zeplin',
-          'InvisionApp'
-        ],
-        database:[
-          'MySQL',
-          'MongoDB'
-        ],
-        others: [
-          'Version control GIT',
-          'IoT - Arduino',
-          'Google Cloud Platform',
-          'MindMeister'
-        ]
-      },
-      contact: {
-        email: 'quangdat2000.pham@gmail.com',
-        phone: '+84383545450',
-        github: 'https://github.com/quangdatpham',
-        linkedin: 'https://www.linkedin.com/in/quang-dat-pham-895235163/'
-      }
-    }
+    author
   });
 });
 
